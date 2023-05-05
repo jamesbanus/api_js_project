26e08f6461469215b229fbd597f42e7ef88ee8d6
James Banus <jamesbanus@jamess-mbp.home>
2023-05-05 13:21:25 +0100

    filtering actually works

--- football.js
+++ football.js
@@ -5,28 +5,37 @@ const apiURL =
 //   "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/competitions/PL/standings";
 //   "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/persons/99813/matches";
 
+let apiData;
+
 async function football() {
   const { data } = await axios.get(apiURL, {
     headers: {
       "X-Auth-Token": "578ac68774834c4a82e28093ac0183a6",
-      origin: "x-requested-with",
+      Origin: "X-Requested-With",
     },
   });
   console.log(data);
+  apiData = data;
   return data;
 }
 
-const apiData = football();
+football();
 
 const dropdownList = document.getElementById("football-team");
 const selectedTeam = dropdownList.value;
+console.log(selectedTeam);
+
 dropdownList.onchange = (e) => {
   const selectedTeam = dropdownList.value;
   console.log(selectedTeam);
   console.log(apiData);
-  //   const filteredData = apiData.filter((teams) =>
-  //     teams.name.includes(selectedTeam)
-  //   );
-  //   console.log(filteredData);
+  // const filterTest = apiData.list.map((item) => {
+  //   return item.teams[0].name;
+  // });
+  // console.log(filterTest);
+
+  const filteredData = apiData.teams.find((team) =>
+    team.name.includes(selectedTeam)
+  );
+  console.log(filteredData);
 };
-console.log(selectedTeam);
